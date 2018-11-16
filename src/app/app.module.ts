acac25f5fc9e32ae6c0757a15073aa6a04917207
Wojciech Kaspera <wojciech.kaspera@gmail.com>
2018-11-16 20:59:16 +0100

    cleaned

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ParentComponent } from './parent-component/parent.component';
-import { ChildComponent } from './child-component/child.component';
-import { SecondChildComponent } from './second-child/second-child.component';
 import { CommunicateService } from './communicate.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ParentComponent,
-    ChildComponent,
-    SecondChildComponent,
   ],
   imports: [
     BrowserModule,
